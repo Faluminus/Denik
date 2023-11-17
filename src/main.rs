42a3fdbea392599b28a464fb2a2aaf79867d49b9
Faluminus <semradfilip@email.cz>
2023-11-17 21:34:01 +0200

    bunch of changes , added Date and Data struct

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,91 @@ use std::io;
 use std::io::Write;
 use std::collections::LinkedList;
 
+struct Date{
+    year:i32,
+    month: i32,
+    day: i32
+}
+impl Date{
+    fn new(year:i32,month:i32,day: i32) -> Date{
+        Date{
+            year,
+            month,
+            day
+        }
+    }
+    fn format_date(&mut self){
+        let month_arr:[i32;12] = [31,28,31,30,31,30,31,31,30,31,30,31];
+        if self.month < 1 || self.month > 12{
+            panic!();
+        }
+
+        if self.day != month_arr[(self.month - 1) as usize]{
+            panic!();
+        }
+    }
+}
+struct Data{
+    datum: Date,
+    value: String,
+}
+impl Data{
+    fn new() -> Data{
+        let datum:Date = Date::new(0,0,0);
+        let value = String::from("");
+        Data{
+            datum,
+            value
+        }
+    }
+    fn get_date(&mut self,year:i32,month:i32,day:i32){
+        self.datum = Date::new(year,month,day);
+        self.datum.format_date();
+    }
+    fn get_value(&mut self,text:String){
+        self.value = text;
+    }
+}
 fn main(){
     let mut input:String = String::new();
-    let cmd = String::from("commands");
-    let exit_code = real_main();
-    let mut zaznam = String::new();
+    let exit_code = 0;
     loop{
+        print_commands();
         print!("->");
         io::stdout().flush().unwrap();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read input");
-        zaznam = String::from(match input.trim(){
-            "commands" => print_commands(),
+        match input.to_lowercase().trim(){
             "zavri" => std::process::exit(exit_code),
             "predchozi" => predchozi(),
             "dalsi" => dalsi(),
-             _ => ""
-        });
+            "novy" => novy(),
+            "uloz" => uloz(),
+            "smaz" => smaz(),
+             _ => ()
+        }
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     }
 }
+
 fn dalsi(){
-    String::from("n");
+
 }
 fn predchozi(){
-    "n";
+
 }
-fn real_main() -> i32{
-    0
+fn novy(){
+
+}
+fn uloz(){
+
+}
+fn smaz(){
+
 }
 fn print_commands(){
+    println!("----------------");
     println!("Commands:");
     println!("  Predchozi");
     println!("  Dalsi");
@@ -39,7 +94,8 @@ fn print_commands(){
     println!("  Uloz");
     println!("  Smaz");
     println!("  Zavri");
-    "";
+    println!("----------------");
+
 }
 
 
