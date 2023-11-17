75014e8aac03c34399ac7f86521a6ffc5ae3400c
Faluminus <semradfilip@email.cz>
2023-11-17 18:11:00 +0100

    Fifu

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,45 @@
-use std::mem;
-use std::ptr;
+use std::io;
+use std::io::Write;
+use std::collections::LinkedList;
 
-pub struct LinkedList<T>(Option<Box<LinkedList<T>>>,Option<(T,Box<LinkedList<T>>)>);
-impl<T> LinkedList<T>{
-    pub fn new() -> Self{
-        LinkedList(None,None)
-    }
-    pub fn push_front(&mut self, data:T){
-        let t = self.0.take();
-        self.0 = Some((data,Box::new(LinkedList(,t))));
+fn main(){
+    let mut input:String = String::new();
+    let cmd = String::from("commands");
+    let exit_code = real_main();
+    let mut zaznam = String::new();
+    loop{
+        print!("->");
+        io::stdout().flush().unwrap();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read input");
+        zaznam = String::from(match input.trim(){
+            "commands" => print_commands(),
+            "zavri" => std::process::exit(exit_code),
+            "predchozi" => predchozi(),
+            "dalsi" => dalsi(),
+             _ => ""
+        });
     }
 }
-fn main() {
-
+fn dalsi(){
+    String::from("n");
+}
+fn predchozi(){
+    "n";
+}
+fn real_main() -> i32{
+    0
+}
+fn print_commands(){
+    println!("Commands:");
+    println!("  Predchozi");
+    println!("  Dalsi");
+    println!("  Novy");
+    println!("  Uloz");
+    println!("  Smaz");
+    println!("  Zavri");
+    "";
 }
 
 
